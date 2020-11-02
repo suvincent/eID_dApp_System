ee20180a409e800bb7f140d1b165264d6e9ef316
dclay0324 <dclay0324@gmail.com>
2020-11-02 22:52:21 +0800

    demo

--- pages/Academic/verifyPage/index.js
+++ pages/Academic/verifyPage/index.js
@@ -4,12 +4,17 @@ import { Link, Router } from '../../../routes';
 import Layout from '../../../components/Layout';
 import web3 from '../../../ethereum/academic/web3';
 import verify from '../../../ethereum/academic/verify';
+import Entity from '../../../ethereum/academic/build/Entity.json'
 
 class getIndex extends Component {
     state = {
       studentEntity: '',
       schoolEntity: '',
       errorMessage: '',
+      errorMessage_isSchool: '',
+      errorMessage_graduted: '',
+      errorMessage_expired: '',
+      errorMessage_canceled: '',
       loading_verify: false,
       open: false
     }
@@ -36,24 +41,37 @@ class getIndex extends Component {
     this.setState({ loading_verify: true, errorMessage: '' });
     try {
       const accounts = await web3.eth.getAccounts();
-
-      let flag;
-
-      flag = await verify.methods.verifyIsSchool(this.state.schoolEntity).call();
-      console.log(flag);
-      if (!flag) throw "The School is NOT Certificated"
-
-      flag = await verify.methods.existence(this.state.studentEntity, this.state.schoolEntity).call();
-      console.log(flag);
-      if (!flag) throw "The Student is Not Graduated"
       
-      flag = await verify.methods.expired(this.state.studentEntity, this.state.schoolEntity).call();
-      console.log(flag);
-      if (!flag) throw "The Certificate was Expired"
+      const student = await new web3.eth.Contract(Entity.abi, this.state.studentEntity);
+      const text = await student.methods.MarkupsSender(this.state.schoolEntity, "diploma").call();
+      console.log(text);
 
-      flag = await verify.methods.verifyMarkup(this.state.studentEntity, this.state.schoolEntity).call();
-      console.log(flag);
-      if (!flag) throw "The Certificate was Canceled"
+      let flag_isSchool;
+      let flag_graduted;
+      let flag_expired;
+      let flag_canceled;
+
+      flag_isSchool = await verify.methods.verifyIsSchool(this.state.schoolEntity).call();
+      console.log(flag_isSchool);
+      //if (!flag_isSchool) throw "The School is NOT Certificated"
+      if (!flag_isSchool) this.setState({ errorMessage_isSchool: "The School is NOT Certificated" })
+
+      flag_graduted = await verify.methods.existence(this.state.studentEntity, this.state.schoolEntity).call();
+      console.log(flag_graduted);
+      //if (!flag_graduted) throw "The Student is Not Graduated"
+      if (!flag_graduted) this.setState({ errorMessage_graduted: "The Student is Not Graduated" })
+      
+      flag_expired = await verify.methods.expired(this.state.studentEntity, this.state.schoolEntity).call();
+      console.log(flag_expired);
+      //if (!flag_expired) throw "The Certificate was Expired"
+      if (!flag_expired) this.setState({ errorMessage_expired: "The Certificate was Expired" })
+
+      flag_canceled = await verify.methods.verifyMarkup(this.state.studentEntity, this.state.schoolEntity).call();
+      console.log(flag_canceled);
+      //if (!flag_canceled) throw "The Certificate was Canceled"
+      if (!flag_canceled) this.setState({ errorMessage_canceled: "The Certificate was Canceled" })
+
+      if (!flag_isSchool || !flag_graduted || !flag_expired || !flag_canceled) throw "NOT Passed"
 
       this.setState( { open: true } );
     } catch (err) {
@@ -115,7 +133,17 @@ You can download the student's certificate from IPFS`}
             onCancel={this.handleCancel}
             onConfirm={this.handleConfirm}
           />
-          <Message error header="Oops!" content={this.state.errorMessage} />
+          {/* <Message error header="Oops!" content={this.state.errorMessage} /> */}
+          <Message
+            error
+            header='The Verify is NOT Passed!!!'
+            list={[
+              this.state.errorMessage_isSchool,
+              this.state.errorMessage_graduted,
+              this.state.errorMessage_expired,
+              this.state.errorMessage_canceled,
+            ]}
+          />
         </Form>
       </Layout>
     );
