b035de465b2e619f5f7adc43886bc94fa2be7f49
106070033 <suvincent0226@gapp.nthu.edu.tw>
2021-07-13 13:11:41 +0800

    docker version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eID_dApp_System
 URL: http://140.114.71.52:3000/
+
+# docker pull
+```
+docker pull coolsu/eid-dapp-system:latest
+```
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 
-/*const routes = require('./routes')
+const routes = require('./routes')
 const express = require('express')
 const next = require('next')
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -22,17 +22,17 @@ app.prepare()
     console.log(`> Ready on http://localhost:${port}`)
   })
 })
-*/
-const {createServer} = require('http');
-const next = require('next');
-const app = next({
-    dev: process.env.NODE_ENV !== 'production'
-});
-const routes = require('./routes');
-const handler = routes.getRequestHandler(app);
-app.prepare().then(()=>{
-    createServer(handler).listen(3000,err =>{
-        if(err)throw err;
-        console.log('Ready on local host');
-    });
-});
+
+// const {createServer} = require('http');
+// const next = require('next');
+// const app = next({
+//     dev: process.env.NODE_ENV !== 'production'
+// });
+// const routes = require('./routes');
+// const handler = routes.getRequestHandler(app);
+// app.prepare().then(()=>{
+//     createServer(handler).listen(3000,err =>{
+//         if(err)throw err;
+//         console.log('Ready on local host');
+//     });
+// });
